e35627ef092ac0d9a1a48f1cddf216517c621036
Robert Elwell <robert@wikia-inc.com>
2013-11-13 11:40:29 -0800

    initial commit for lib

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="corenlp-xml-lib",
+    version= "0.0.1",
+    author = "Robert Elwell",
+    author_email = "robert.elwell@gmail.com",
+    description = "Library for interacting with the XML output of the Stanford CoreNLP pipeline.",
+    license = "Other",
+    packages = ["corenlp-xml"],
+    depends = ["lxml"]
+    )
