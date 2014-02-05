726b350413cf62edc5f97365d92398afb4e0e59e
Robert Elwell <robert@wikia-inc.com>
2014-02-05 09:43:07 -0800

    updated setup.py stuff

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name="corenlp-xml-lib",
-    version= "0.0.1",
-    author = "Robert Elwell",
-    author_email = "robert.elwell@gmail.com",
-    description = "Library for interacting with the XML output of the Stanford CoreNLP pipeline.",
-    license = "Other",
-    packages = ["corenlp_xml"],
-    depends = ["lxml"]
+    version="0.0.1",
+    author="Robert Elwell",
+    author_email="robert.elwell@gmail.com",
+    description="Library for interacting with the XML output of the Stanford CoreNLP pipeline.",
+    license="Other",
+    packages=["corenlp_xml"],
+    install_requires=["lxml", "nltk"]
     )
