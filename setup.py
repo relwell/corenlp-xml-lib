a94bbcd8fadfeb839ddb910c89a6104e140b2178
Robert Elwell <robert@wikia-inc.com>
2014-02-05 09:44:49 -0800

    requirements.txt-based setup.py requires

--- setup.py
+++ setup.py
@@ -1,5 +1,14 @@
 from setuptools import setup
 
+from pip.req import parse_requirements
+
+# parse_requirements() returns generator of pip.req.InstallRequirement objects
+install_reqs = parse_requirements('requirements.txt')
+
+# reqs is a list of requirement
+# e.g. ['django==1.5.1', 'mezzanine==1.4.6']
+reqs = [str(ir.req) for ir in install_reqs]
+
 setup(
     name="corenlp-xml-lib",
     version="0.0.1",
@@ -8,5 +17,5 @@ setup(
     description="Library for interacting with the XML output of the Stanford CoreNLP pipeline.",
     license="Other",
     packages=["corenlp_xml"],
-    install_requires=["lxml", "nltk"]
+    install_requires=reqs
     )
