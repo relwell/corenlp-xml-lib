78c1593e1978b05d2b83d7e769e16562082a7545
Robert Elwell <robert@wikia-inc.com>
2014-06-04 14:11:25 -0700

    added url to github repo

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     author="Robert Elwell",
     author_email="robert.elwell@gmail.com",
     description="Library for interacting with the XML output of the Stanford CoreNLP pipeline.",
+    url="https://github.com/relwell/corenlp-xml-lib",
     license="Other",
     packages=["corenlp_xml"],
     install_requires=["PyYAML>=3.10", "bidict>=0.1.1", "lxml>=3.2.4", "nltk>=2.0.4", "wsgiref>=0.1.2"]
