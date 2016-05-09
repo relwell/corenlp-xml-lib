b658ccc5d54901f4d4bcf6a540d7ef69fb853e78
Wouter van Atteveldt <wouter@vanatteveldt.com>
2016-05-09 21:00:14 +0200

    Remove wsgiref requirement
    I don't think it was actually used anywhere (?) and it caused troubles with python3

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name="corenlp-xml",
-    version="1.0.2",
+    version="1.0.3",
     author="Robert Elwell",
     author_email="robert.elwell@gmail.com",
     description="Library for interacting with the XML output of the Stanford CoreNLP pipeline.",
     url="https://github.com/relwell/corenlp-xml-lib",
     license="Other",
     packages=["corenlp_xml"],
-    install_requires=["PyYAML>=3.10", "bidict>=0.1.1", "lxml>=3.2.4", "nltk>=2.0.4", "wsgiref>=0.1.2"]
+    install_requires=["PyYAML>=3.10", "bidict>=0.1.1", "lxml>=3.2.4", "nltk>=2.0.4"]
     )
